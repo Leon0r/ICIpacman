5dc5511d941cd4b42e373b577a11626b6d7198bd
Unknown <leonorcu@ucm.es>
2019-09-13 23:14:13 +0200

    Initial commit project stuff

---
+++ PacManEngine/src/main/java/pacman/controllers/MsPacmanController.java
@@ -0,0 +1,26 @@
+package pacman.controllers;
+
+import java.util.Random;
+
+import pacman.game.Constants.GHOST;
+import pacman.game.Constants.MOVE;
+import pacman.game.Game;
+import pacman.game.internal.Node;
+
+
+public final class MsPacmanController extends PacmanController {
+
+	private Random rnd  = new Random();
+	private MOVE[] allMoves = MOVE.values();
+	
+	@Override
+	public MOVE getMove(Game game, long timeDue) {
+		if(getMove() == null)
+			allMoves = game.getPossibleMoves(game.getPacmanCurrentNodeIndex());
+		else
+			allMoves = game.getPossibleMoves(game.getPacmanCurrentNodeIndex(), getMove());
+		
+		lastMove = allMoves[rnd.nextInt(allMoves.length)];
+		return lastMove;
+	}
+}
