6c203d662ef6b22b7e8b910f36945c055796be30
Unknown <leonorcu@ucm.es>
2019-09-14 18:32:47 +0200

    ghosts

--- PacManEngine/src/main/java/es/ucm/fdi/ici/c1920/practica0/grupoYY/Ghosts.java
+++ PacManEngine/src/main/java/es/ucm/fdi/ici/c1920/practica0/grupoYY/Ghosts.java
@@ -1,5 +1,29 @@
 package es.ucm.fdi.ici.c1920.practica0.grupoYY;
 
-public class Ghosts {
+import java.util.EnumMap;
 
+import pacman.controllers.GhostController;
+import pacman.game.Game;
+import pacman.game.Constants.DM;
+import pacman.game.Constants.GHOST;
+import pacman.game.Constants.MOVE;
+
+public class Ghosts extends GhostController{
+	private EnumMap<GHOST, MOVE> moves = new EnumMap<GHOST, MOVE>(GHOST.class);
+	//private MOVE[] allMoves = MOVE.values();
+	private DM[] allDM = DM.values();
+
+	@Override
+	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
+		// TODO Auto-generated method stub
+		moves.clear();
+		for(GHOST ghostType : GHOST.values()) {
+			if(game.doesGhostRequireAction(ghostType)) {
+				MOVE mov = game.getNextMoveTowardsTarget(game.getGhostCurrentNodeIndex(ghostType), game.getPacmanCurrentNodeIndex(), allDM[0]);
+				//PATH, EUCLID, MANHATTAN
+				moves.put(ghostType,  mov);
+			}
+		}
+		return moves;
+	}
 }
